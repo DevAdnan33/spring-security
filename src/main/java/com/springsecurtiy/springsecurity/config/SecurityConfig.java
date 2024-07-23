70cad90cad981033a8c32b7305ab78fa5b9b24dd
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-23 23:21:51 +0500

    In Memory Authentication

--- src/main/java/com/springsecurtiy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/springsecurtiy/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +24,13 @@ public class SecurityConfig {
         // http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    // In memory authentication
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("USER").build();
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
