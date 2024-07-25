ef0a928d45e150c6600c37bfff0a72af25521fb7
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-25 20:28:48 +0500

    Added Password Encoder

--- src/main/java/com/springsecurtiy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/springsecurtiy/springsecurity/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.springsecurtiy.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,13 +14,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity // for role base authentication
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -30,14 +40,28 @@ public class SecurityConfig {
             frameOptionsConfig.sameOrigin();
         }));
         http.csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
     // In memory authentication
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
-        UserDetails admin = User.withUsername("user2").password("{noop}password1").roles("USER").build();
-        return new InMemoryUserDetailsManager(user1, admin);
+        UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("password1")).roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("user2").password(passwordEncoder().encode("password2")).roles("ADMIN")
+                .build();
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+        // return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
